047f7cb2ddbf10730b76bfb3af94f1b2b536d4c1
Дарья Мальцева <helolo121003@mail.ru>
2023-09-13 17:16:04 +0700

    Перенаправление

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, redirect
 app = Flask(__name__)
 
 @app.route("/")
 @app.route("/index")
 def start():
-    return """
+    return redirect("/menu", code=302)
+
+@app.route("/menu")
+def menu():
+        return """
 <!doctype html>
 <html>
     <head>
@@ -29,6 +33,7 @@ def start():
     </body>
 </html>
 """
+
 @app.route("/lab1")
 def lab1():
     return """
