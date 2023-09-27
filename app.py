0c8d6922b5339202d9869f599e302f879663741c
Дарья Мальцева <helolo121003@mail.ru>
2023-09-27 17:18:51 +0700

    Наследование шаблонов

--- app.py
+++ app.py
@@ -211,3 +211,6 @@ def example():
     return render_template('example.html', name=name, number_lab=number_lab, 
             group=group, number_course=number_course, fruits=fruits, books=books)
 
+@app.route("/lab2/")
+def lab2():
+    return render_template('lab2.html')
