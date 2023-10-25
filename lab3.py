bcff14019f960ff98927852917ef5b99badcfc19
Дарья Мальцева <helolo121003@mail.ru>
2023-10-25 12:47:22 +0700

    заказ напитка

--- lab3.py
+++ lab3.py
@@ -6,6 +6,7 @@ lab3 = Blueprint('lab3',__name__)
 def lab():
     return render_template('lab3.html')
 
+
 @lab3.route("/lab3/forml")
 def forml():
     errors = {}
@@ -19,3 +20,31 @@ def forml():
 
     sex = request.args.get('sex')
     return render_template('form1.html', user=user, age=age, sex=sex, errors=errors)
+
+
+@lab3.route("/lab3/order")
+def order():
+    return render_template('order.html')
+
+@lab3.route("/lab3/pay")
+def pay():
+    price = 0
+    drink = request.args.get('drink')
+    if drink == 'cofee':
+        price = 120
+    elif drink == 'black-tea':
+       price = 80
+    else:
+       price = 70  
+
+    if request.args.get('milk') == 'on':
+       price += 30
+    if request.args.get('sugar') == 'on':
+       price += 10
+
+    return render_template('pay.html', price = price)    
+
+
+@lab3.route('/lab3/success')
+def success():
+        return render_template('thx.html')
