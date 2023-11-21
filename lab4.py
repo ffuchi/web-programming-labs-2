daae46140beebed59961ded33c4b16dbe6919d84
Дарья Мальцева <helolo121003@mail.ru>
2023-11-22 01:28:07 +0700

    заказ зерна

--- lab4.py
+++ lab4.py
@@ -44,8 +44,6 @@ def fridge():
     elif (int(temp) > -1):
         error = 'не удалось установить температуру — слишком высокое значение'
         return render_template('fridge.html', error=error, temp=temp)
-    # else:
-    #     return render_template('temp.html', error=error, temp=temp)
 
     elif (int(temp) >= -12 and int(temp) <= -9):
         error = f'Установлена температура: {temp}°С ❄️❄️❄️'
@@ -56,3 +54,45 @@ def fridge():
     elif (int(temp) >= -4 and int(temp) <= -1):
         error = f'Установлена температура: {temp}°С ❄️'
         return render_template('temp.html', error=error, temp=temp)    
+
+
+@lab4.route("/lab4/seed", methods = ['GET', 'POST'])
+def seed():
+    price = 0
+    if request.method == 'GET':
+        return render_template('seed.html')
+    seed = request.form.get('seed')
+    weight = request.form.get('weight')
+
+    if seed == 'ячмень':
+        price = 12000
+    elif seed == 'пшеница':
+        price = 8500
+    elif seed == 'овёс':
+        price = 8700   
+    else:
+        price = 14000  
+
+    if not weight :
+        error = 'Введите вес'
+        return render_template('seed.html', error=error, weight=weight, price=price)
+
+    else:
+        weight=int(weight)
+        if weight > 50 and weight < 500 and weight == 500:
+            error='Применена скидка 10% за большой объем'
+            price = (price * weight) * 0.9
+            return render_template('receipt.html', price=price, error=error, weight=weight)
+        elif weight > 500:
+            error='Такого объёма сейчас нет в наличии'
+            return render_template('seed.html', price=price, error=error, weight=weight)
+        elif weight < 0 or weight == 0:
+            error='Неверное значение веса'
+            return render_template('seed.html', price=price, error=error, weight=weight)
+        else:
+            error=''
+            price = price * weight
+            return render_template('receipt.html', price=price, error=error, weight=weight)
+      
+
+    
