eb605237da003cde173b00da923e1b367e4c4680
Дарья Мальцева <helolo121003@mail.ru>
2023-12-19 19:05:19 +0700

    заготовка 7-ой лабораторной

--- lab1.py
+++ lab1.py
@@ -30,6 +30,8 @@ def menu():
                 <li><a href="/lab3/" target="_blank">Лабораторная работа 3</a></li>
                 <li><a href="/lab4/" target="_blank">Лабораторная работа 4</a></li>
                 <li><a href="/lab5" target="_blank">Лабораторная работа 5</a></li>
+                <li><a href="/lab6" target="_blank">Лабораторная работа 6</a></li>
+                <li><a href="/lab7/" target="_blank">Лабораторная работа 7</a></li>
             </ol>
         </main>
     
