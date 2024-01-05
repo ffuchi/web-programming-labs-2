b94053cff9d5d33bf7e9d45a929a73e2056b7d92
Дарья Мальцева <helolo121003@mail.ru>
2024-01-06 04:32:56 +0700

    Обработчик ошибки 500

--- lab9.py
+++ lab9.py
@@ -4,8 +4,17 @@ from flask import render_template, Blueprint, request, abort, jsonify
 lab9 = Blueprint("lab9", __name__)
 
 @lab9.app_errorhandler(404)
-def not_found(e):
-    return render_template('lab9/error.html'), 404
+def not_found(err):
+    return render_template('lab9/error404.html'), 404
+
+@lab9.app_errorhandler(500)
+def server_error(err):
+    return render_template('lab9/error500.html'), 500
+
+
+@lab9.route('/lab9/500')
+def server_error():
+    return render_template('lab9/error500.html')
 
 
 @lab9.route("/lab9/")
